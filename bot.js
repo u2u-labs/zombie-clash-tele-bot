3509bd9b3bd7e184acfb7a55c5639ef69bff6dea
vietthai244 <thai.toviet@gmail.com>
2025-04-16 15:15:28 +0700

    update auth link

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ bot.command('start', (ctx) => {
   ctx.reply("Log in using LayerG Universal Account:", {
     reply_markup: {
       inline_keyboard: [[
-        { text: "Verify", url: "https://t.me/layerg_ua_verification_stg_bot?start=login-1-49C29C6B716aAf9fFBDDc548448A7f6dAe187A06" }
+        { text: "Verify", url: "https://t.me/layerg_ua_verification_stg_bot?start=login-4-49C29C6B716aAf9fFBDDc548448A7f6dAe187A06" }
       ]]
     }
   });
